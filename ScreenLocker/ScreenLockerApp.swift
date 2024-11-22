2dd9f4835a2a59b7b5e14de8e20c9900048a1cf7
Alex <a.d.koryakin@gmail.com>
2024-11-22 11:21:50 +0200

    Initial Commit

---
+++ ScreenLocker/ScreenLockerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScreenLockerApp.swift
+//  ScreenLocker
+//
+//  Created by Alex on 22.11.2024.
+//
+
+import SwiftUI
+
+@main
+struct ScreenLockerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
